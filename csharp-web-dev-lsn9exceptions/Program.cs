f6372e30b00e71a95294b1c6b3e46e3f46e05ec7
NestoMedina <emedina670@hotmail.com>
2021-06-22 23:15:17 -0400

    finished assignment

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -7,21 +7,60 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            try
+            {
+                if (y == 0)
+                {
+                    throw new ArgumentOutOfRangeException("Values cannot be 0");
+                }
+                else
+                {
+                    Console.WriteLine(x / y);
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                y = 1;
+                Console.WriteLine(e);
+            }
+
+            return x / y;
         }
 
+        //receive string parse final output as int
+        //if string ends in .cs they get 1 point, no .cs 0 points
+        //if string is empty or null throw exception
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
-        }
-        */
+            int i = 0;
+            try
+            {
+                if (fileName.Contains(".cs") == true)
+                {
+                    i++;
+                }
+                else if (fileName == null || fileName == "")
+                {
+                    throw new ArgumentNullException("No program was entered");
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine($"This program receives {i} point(s).");
+            return i;
+            }
+        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+
+            //(2, 0);
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,6 +69,7 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            CheckFileExtension("program");
 
         }
     }
